d1ad652ff15457be515dc8e81b67128468b92668
Sebastian Mendez <sebas.mendezg@gmail.com>
2025-05-14 13:12:20 -0500

    deploy gh

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // 👇 Asegúrate de que esta línea tenga el nombre del repo correctamente:
 export default defineConfig({
-  base: '/clasc/', // 👈 MUY IMPORTANTE
+  base: 'clasc.co', // Cambia a '/' para dominio personalizado
   plugins: [react()]
 })
