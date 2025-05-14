fb4a31b6673b0b813684ae45e2a1d0da7d213873
Sebastian Mendez <sebas.mendezg@gmail.com>
2025-05-14 11:56:39 -0500

    cambios types

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 Asegúrate de que esta línea tenga el nombre del repo correctamente:
 export default defineConfig({
-  base: '/clasc/', // 
-  plugins: [react()],
-})
+  base: '/clasc/', // 👈 MUY IMPORTANTE
