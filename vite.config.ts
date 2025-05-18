9c20b65fcc9ab002fea3d8426de22c2542b85c7a
Sebastian Mendez <sebas.mendezg@gmail.com>
2025-05-18 18:40:26 -0500

    cambios dist a docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // 👇 Asegúrate de que esta línea tenga el nombre del repo correctamente:
 export default defineConfig({
-  base: 'clasc.co', // Cambia a '/' para dominio personalizado
-  plugins: [react()]
+  base: '', 
+  plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
